bc6a758330c58a5a165b69d84bf8fd627c917868
ahmadjon2008 <robotoprotecno2008@gmail.com>
2024-01-21 09:35:02 +0500

    Wikipedia Bot

---
+++ main.py
@@ -0,0 +1,39 @@
+import wikipedia
+import logging
+import asyncio
+import sys
+from config import Token
+from aiogram.types import Message
+from aiogram.filters.command import Command
+from aiogram import Bot, types, Dispatcher
+
+bot = Bot(Token)
+dp = Dispatcher()
+
+
+@dp.message(Command("start"))
+async def start(message: Message):
+    await message.answer("Asslomu aleykum\nMen sizga wikipedia saytidan ma'lumot olib beruvchi botman.")
+
+
+@dp.message(Command("help"))
+async def help_command(message: Message):
+    await message.answer("Bu bot sizga wikipediadan ma'lumot olib beradi,\nNima haqida ma'lumot olishni istaysiz ?")
+
+
+@dp.message()
+async def wiki(message: Message):
+    try:
+        respond = wikipedia.summary(message.text)
+        await message.reply(f"<pre>{respond}</pre>", parse_mode="HTML")
+    except TypeError:
+        await message.reply("Siz qidirgan mavzu topilmadi,\nQaytadan urunib ko'ring")
+
+
+async def main() -> None:
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
